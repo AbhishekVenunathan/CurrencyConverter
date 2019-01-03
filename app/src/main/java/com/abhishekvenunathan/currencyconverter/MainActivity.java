5bcf0da3c858de5cb29fb6c7f6be9c9d090b74b0
AbhishekVenunathan <abhishek.venunathan@gmail.com>
2019-01-03 19:53:17 +0530

    Initial commit

---
+++ app/src/main/java/com/abhishekvenunathan/currencyconverter/MainActivity.java
@@ -0,0 +1,27 @@
+package com.abhishekvenunathan.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+        EditText dollarField = findViewById(R.id.dollarField);
+
+        Double dollarAmount= Double.parseDouble(dollarField.getText().toString());
+        Double poundAmount= dollarAmount*0.65;
+
+        Toast.makeText(getApplicationContext(),poundAmount.toString(),Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
